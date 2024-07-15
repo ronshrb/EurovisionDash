b947b6482da8c77ca66f80271f98ad529859b1bd
ronshrb <134402974+ronshrb@users.noreply.github.com>
2024-07-15 14:58:56 +0300

    Update scatter_viso.py

--- scatter_viso.py
+++ scatter_viso.py
@@ -3,13 +3,6 @@ import plotly.graph_objects as go
 from scipy.stats import trim_mean
 
 def create_viso(df, mode='Total Score'):
-    if mode == 'Total Score':
-        voters = 'final_total_points'
-    elif mode == 'Jury Score':
-        voters = 'final_jury_points'
-    elif mode == 'Televote Score':
-        voters = 'final_televote_points'
-
     scores = df
     query = """
         SELECT
@@ -28,50 +21,141 @@ def create_viso(df, mode='Total Score'):
     df = score_post
     df['final_draw_position'] = df['final_draw_position'].astype(int)
 
-    # Calculate trimmed mean points and places for each draw position
-    trimmed_mean_score = df.groupby('final_draw_position')[voters].apply(
-        lambda x: round(trim_mean(x, proportiontocut=0.1),2)).reset_index()
+    if mode == 'Total Score':
+        # Calculate trimmed mean points and places for each draw position
+        trimmed_mean_score = df.groupby('final_draw_position')['final_total_points'].apply(
+            lambda x: round(trim_mean(x, proportiontocut=0.1), 2)).reset_index()
 
-    # Sort the DataFrames by final_draw_position
-    df_sorted = df.sort_values('final_draw_position')
-    trimmed_mean_score_sorted = trimmed_mean_score.sort_values('final_draw_position')
+        # Sort the DataFrames by final_draw_position
+        df_sorted = df.sort_values('final_draw_position')
+        trimmed_mean_score_sorted = trimmed_mean_score.sort_values('final_draw_position')
 
-    fig = go.Figure()
+        fig = go.Figure()
 
-    fig.add_trace(go.Scatter(
-        x=df_sorted['final_draw_position'],
-        y=df_sorted[voters],
-        mode='markers',
-        marker=dict(
-            size=10,
-            color='green',
-            opacity=0.6,
-        ),
-        text=[f'Country: {country}<br>Year: {year}<br>Running Position: {draw_position}<br>Final Place: {place}'
-              for country, year, draw_position, place in
-              zip(df_sorted['country'], df_sorted['year'], df_sorted['final_draw_position'], df_sorted[voters])],
-        hoverinfo='text',
-        name='Individual Score'
-    ))
+        fig.add_trace(go.Scatter(
+            x=df_sorted['final_draw_position'],
+            y=df_sorted['final_total_points'],
+            mode='markers',
+            marker=dict(
+                size=10,
+                color='#5aae61',
+                opacity=0.6,
+            ),
+            text=[f'Country: {country}<br>Year: {year}<br>Running Position: {draw_position}<br>Final Place: {place}'
+                  for country, year, draw_position, place in
+                  zip(df_sorted['country'], df_sorted['year'], df_sorted['final_draw_position'],
+                      df_sorted['final_total_points'])],
+            hoverinfo='text',
+            name='Individual Total Score'
+        ))
 
-    fig.add_trace(go.Scatter(
-        x=trimmed_mean_score_sorted['final_draw_position'],
-        y=trimmed_mean_score_sorted[voters],
-        mode='lines+markers',
-        marker=dict(
-            size=10,
-            color='orange',
-        ),
-        line=dict(
-            width=2,
-            color='orange',
-        ),
-        text=[f'Running Position: {draw_position}<br>Trimmed Mean Final Place: {place}'
-              for draw_position, place in
-              zip(trimmed_mean_score_sorted['final_draw_position'], trimmed_mean_score_sorted[voters])],
-        hoverinfo='text',
-        name='Trimmed Mean Score'
-    ))
+        fig.add_trace(go.Scatter(
+            x=trimmed_mean_score_sorted['final_draw_position'],
+            y=trimmed_mean_score_sorted['final_total_points'],
+            mode='lines+markers',
+            marker=dict(
+                size=10,
+                color='#1b7837',
+            ),
+            line=dict(
+                width=2,
+                color='#1b7837',
+            ),
+            text=[f'Running Position: {draw_position}<br>Trimmed Mean Final Place: {place}'
+                  for draw_position, place in
+                  zip(trimmed_mean_score_sorted['final_draw_position'],
+                      trimmed_mean_score_sorted['final_total_points'])],
+            hoverinfo='text',
+            name='Trimmed Mean Total Score'
+        ))
+    else:
+        # Calculate trimmed mean points and places for each draw position
+        trimmed_mean_score = df.groupby('final_draw_position')['final_televote_points'].apply(
+            lambda x: round(trim_mean(x, proportiontocut=0.1),2)).reset_index()
+
+        # Sort the DataFrames by final_draw_position
+        df_sorted = df.sort_values('final_draw_position')
+        trimmed_mean_score_sorted = trimmed_mean_score.sort_values('final_draw_position')
+
+        fig = go.Figure()
+
+        fig.add_trace(go.Scatter(
+            x=df_sorted['final_draw_position'],
+            y=df_sorted['final_televote_points'],
+            mode='markers',
+            marker=dict(
+                size=10,
+                color='#fdae61',
+                opacity=0.6,
+            ),
+            text=[f'Country: {country}<br>Year: {year}<br>Running Position: {draw_position}<br>Final Place: {place}<br> Type: Televote'
+                  for country, year, draw_position, place in
+                  zip(df_sorted['country'], df_sorted['year'], df_sorted['final_draw_position'], df_sorted['final_televote_points'])],
+            hoverinfo='text',
+            name='Individual Televote Score'
+        ))
+
+
+        fig.add_trace(go.Scatter(
+            x=df_sorted['final_draw_position'],
+            y=df_sorted['final_jury_points'],
+            mode='markers',
+            marker=dict(
+                size=10,
+                color='#7570b3',
+                opacity=0.6,
+            ),
+            text=[f'Country: {country}<br>Year: {year}<br>Running Position: {draw_position}<br>Final Place: {place}<br> Type: Jury'
+                  for country, year, draw_position, place in
+                  zip(df_sorted['country'], df_sorted['year'], df_sorted['final_draw_position'], df_sorted['final_televote_points'])],
+            hoverinfo='text',
+            name='Individual Jury Score'
+        ))
+
+        fig.add_trace(go.Scatter(
+            x=trimmed_mean_score_sorted['final_draw_position'],
+            y=trimmed_mean_score_sorted['final_televote_points'],
+            mode='lines+markers',
+            marker=dict(
+                size=10,
+                color='#e66101',
+            ),
+            line=dict(
+                width=2,
+                color='#e66101',
+            ),
+            text=[f'Running Position: {draw_position}<br>Trimmed Mean Final Place: {place}<br> Type: Televote'
+                  for draw_position, place in
+                  zip(trimmed_mean_score_sorted['final_draw_position'],
+                      trimmed_mean_score_sorted['final_televote_points'])],
+            hoverinfo='text',
+            name='Trimmed Mean Televote Score'
+        ))
+        # Calculate trimmed mean points and places for each draw position
+        trimmed_mean_score = df.groupby('final_draw_position')['final_jury_points'].apply(
+            lambda x: round(trim_mean(x, proportiontocut=0.1),2)).reset_index()
+
+        # Sort the DataFrames by final_draw_position
+        df_sorted = df.sort_values('final_draw_position')
+        trimmed_mean_score_sorted = trimmed_mean_score.sort_values('final_draw_position')
+        fig.add_trace(go.Scatter(
+            x=trimmed_mean_score_sorted['final_draw_position'],
+            y=trimmed_mean_score_sorted['final_jury_points'],
+            mode='lines+markers',
+            marker=dict(
+                size=10,
+                color='#762a83',
+            ),
+            line=dict(
+                width=2,
+                color='#762a83',
+            ),
+            text=[f'Running Position: {draw_position}<br>Trimmed Mean Final Place: {place}<br> Type: Jury'
+                  for draw_position, place in
+                  zip(trimmed_mean_score_sorted['final_draw_position'], trimmed_mean_score_sorted['final_jury_points'])],
+            hoverinfo='text',
+            name='Trimmed Mean Jury Score'
+        ))
 
     fig.update_layout(
         # title='Final Score by Running Order and Type of Votes',
@@ -101,7 +185,7 @@ def create_viso(df, mode='Total Score'):
                 size=15
             ),
             x=0.8,
-            y=1.1,
+            y=1.2,
             bgcolor='rgba(0,0,0,0)',
             xanchor='center',
             yanchor='middle',
