b44858c9859905bdcf1e7826f9e4afa880ac598f
ronshrb <134402974+ronshrb@users.noreply.github.com>
2024-07-16 23:14:28 +0300

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -2,24 +2,26 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from libs.pandasql import sqldf
-import map_viso as mv
 import pie_viso as pv
 import scatter_viso as sv
 import additional_data as ad
+import map_viso as mv2
+import box_viso as bv
 
 st.set_page_config(
     page_title="Eurovision Dashboard",
-    page_icon="icon.png",
+    page_icon = 'icon.png',
     layout="wide",
     initial_sidebar_state="expanded")
 
+# set logo
 col1, col2, col3 = st.columns([1.1,2.4,1])
 with col2:
     image_path = 'logo2.png'
 
     st.image(image_path, caption='', use_column_width=False)
 
-
+# set df for search
 url_list = [{'Song Name': song, 'URL': details['url']} for song, details in ad.urls.items()]
 urls_df = pd.DataFrame(url_list)
 song_data = pd.read_csv('Data/song_data.csv', encoding='latin1')
@@ -41,8 +43,6 @@ query = """
 """
 search_df = sqldf(query, locals())
 
-# main filter for years
-years = [2009, 2010, 2011, 2012, 2014, 2015, 2016, 2017, 2018, 2019, 2021, 2022, 2023]
 
 # Colorblind Mode
 if 'colorblind_mode' not in st.session_state:
@@ -55,10 +55,15 @@ if st.session_state.colorblind_mode:
 else:
     text = 'Colorblind Mode Disabled'
 
-st.button(text, on_click=toggle_colorblind_mode)
-
+col1, col2, col3, col4 = st.columns([1,1,1,0.6])
+with col4:
+    st.markdown("")
+    st.markdown("")
+    st.button(text, on_click=toggle_colorblind_mode)
+# main filter for years
+years = [2009, 2010, 2011, 2012, 2014, 2015, 2016, 2017, 2018, 2019, 2021, 2022, 2023]
 with col1:
-    selected_years = st.sidebar.select_slider(
+    selected_years = st.select_slider(
         'Global filter by range of years:',
         options=years,
         value=(2009, 2023)
@@ -66,11 +71,9 @@ with col1:
 song_data = song_data[song_data['year'].between(selected_years[0], selected_years[1])]
 song_data = song_data[song_data['year'] != 2013]
 
+# songs search engine
 st.sidebar.title("Search for a Song")
-# Filter by year
 selected_year = st.sidebar.selectbox('Select Year:', options=[''] + sorted(df['year'].unique().tolist()))
-
-# Filter by country
 selected_country = st.sidebar.selectbox('Select Country:', options=[''] + sorted(df['country'].unique().tolist()))
 filtered_df = search_df.copy()
 
@@ -85,7 +88,6 @@ if not filtered_df.empty:
         artist= filtered_df[filtered_df['song_name']==selected_song]['artist_name'].values[0]
         country = filtered_df[filtered_df['song_name'] == selected_song]['country'].values[0]
         st.sidebar.write(f"**Information about {selected_song} by {artist} from {country}:**")
-        # st.sidebar.write(f"Artist Song: {filtered_df[filtered_df['song_name']==selected_song]['artist_name'].values[0]}")
         st.sidebar.write(
             f"Year: {filtered_df[filtered_df['song_name'] == selected_song]['year'].values[0]}")
         st.sidebar.write(
@@ -106,42 +108,48 @@ if not filtered_df.empty:
 else:
     st.sidebar.write("No songs match your search criteria.")
 
-
 # visualizations
+col1, col2 = st.columns([1.35,0.65])
+with col1:
+    st.markdown("#### Average Score by Country and Eurovision Debut Year")
+    st.markdown("Shows average total points for each country and their eurovision debut year")
+    dataset_options = ['No Groups', 'Eurovision Debut Year', 'Semi Final']
+    selected_years_for_map = [year for year in range(*selected_years)]
+    selected_years_for_map.append(selected_years[-1])  # add the last year
+    if (2019 in selected_years_for_map) and (2021 in selected_years_for_map):
+        selected_years_for_map.remove(2020)
+    if (2012 in selected_years_for_map) and (2014 in selected_years_for_map):
+        selected_years_for_map.remove(2013)
+with col2:
+    st.markdown("#### Final Score by Semi Final/Host/Top 5")
+    st.markdown("Shows statics of total points by the way the country qualified for the final")
 
-st.markdown("#### Score by Country and Year")
-st.markdown("Shows final scores for each country in each year, and geographical and categorical patterns")
-dataset_options = ['No Groups', 'Eurovision Debut Year', 'Semi Final']
-selected_years_for_map = [year for year in range(*selected_years)]
-selected_years_for_map.append(selected_years[-1])  # add the last year
-if (2022 in selected_years_for_map) and (2021 in selected_years_for_map):
-    selected_years_for_map.remove(2020)
-if (2012 in selected_years_for_map) and (2014 in selected_years_for_map):
-    selected_years_for_map.remove(2013)
+selected_countries = song_data[(song_data['qualified_10'] == '1') | (song_data['qualified_10'] == '-')][
+            'country'].unique()
+selected_countries = np.insert(selected_countries, 0, 'All Countries')
 
-# Display the dropdown and save the selected value
 col1, col2, col3, col4, col5 = st.columns(5)
 with col1:
-    selected_dataset = st.selectbox('Select Group:', dataset_options)
-with col2:
-    selected_year = st.selectbox('Select Year:', selected_years_for_map)
-
-selected_countries = song_data[(song_data['year'] == selected_year) & ((song_data['qualified_10'] == '1') | (song_data['qualified_10'] == '-'))]['country'].unique()
-selected_countries = np.insert(selected_countries, 0, 'All Countries')
-with col3:
     selected_country = st.selectbox('Select country:', selected_countries)
 
-map_viso = mv.create_viso(song_data, selected_year, selected_dataset, selected_country,colorblind_mode=st.session_state.colorblind_mode)
-st.plotly_chart(map_viso, use_container_width=True)
 
+col1, col2 = st.columns([1.35,0.65])
+with col1:
+    map_viso = mv2.create_viso(song_data, selected_year, selected_country,
+                               colorblind_mode=st.session_state.colorblind_mode)
+    st.plotly_chart(map_viso, use_container_width=True)
+with col2:
+    bars_viso = bv.create_viso(song_data, colorblind_mode=st.session_state.colorblind_mode)
+    st.plotly_chart(bars_viso, use_container_width=True)
 
 col1, col2 = st.columns([0.9,1.1])
 with col2:
     st.markdown("#### Scores by Running Order & Voting Type")
     st.markdown("Shows if the running order affects the score given to a country")
 with col1:
-    st.markdown("#### Songs (Inner) & Scores (Outer) by Feature")
+    st.markdown("#### Songs (Inner) & Average Scores (Outer) by Feature")
     st.markdown("Shows which features are the most used and what effect they have on the final score")
+
 
 col1, col2, col3, col4 = st.columns([1,0.7,1,1])
 with col3:
@@ -150,7 +158,7 @@ with col3:
 with col1:
     feature = ["style","BPM","energy","danceability","happiness"]
     selected_feature = st.selectbox('Select Feature:', feature)
-    
+
 pie_viso = pv.create_viso(song_data,selected_feature,colorblind_mode=st.session_state.colorblind_mode)
 col1, col2 = st.columns([0.8,1.2])
 with col1:
@@ -158,4 +166,6 @@ with col1:
 with col2:
     scatter_viso = sv.create_viso(song_data, selected_mode,colorblind_mode=st.session_state.colorblind_mode)
     st.plotly_chart(scatter_viso, use_container_width=True)
+
+
 
