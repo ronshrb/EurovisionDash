aba083703578961a19603db188ef356550828a1e
ronshrb <134402974+ronshrb@users.noreply.github.com>
2024-07-17 00:11:01 +0300

    Update pie_viso.py

--- pie_viso.py
+++ pie_viso.py
@@ -1,37 +1,28 @@
 
 import pandas as pd
 import plotly.graph_objects as go
+from libs.pandasql import sqldf
 
 def create_viso(df, feature='style', colorblind_mode=False):
-    scores = df
-
-    grouped_data = scores.groupby(feature).agg({
-        'final_total_points': ['mean', 'count']
-    }).reset_index()
-
-    grouped_data.columns = [feature, 'Average Score', 'Number of Songs']
-
-    # Calculate standard deviation per group
-    std_dev = scores.groupby(feature)['final_total_points'].std().reset_index()
-    std_dev.columns = [feature, 'Std Dev']
-
-    # Merge standard deviation back to grouped data
-    grouped_data = pd.merge(grouped_data, std_dev, on=feature, how='left')
-
-    # Calculate normalized score
-    grouped_data['Normalized Score'] = grouped_data['Average Score'] / grouped_data['Std Dev'].replace(0, pd.NA)
-
-    # Replace NaN values with 0 (if desired)
-    grouped_data['Normalized Score'] = grouped_data['Normalized Score'].fillna(0)
-    grouped_data['Normalized Score'] = grouped_data['Normalized Score'].round(2)
-    grouped_data['Average Score'] = grouped_data['Average Score'].round(2)
-
     if feature != 'style':
-        grouped_data = grouped_data[(grouped_data[feature] != '-') & (grouped_data[feature].notna())]
-        grouped_data[feature] = grouped_data[feature].astype(float)
-        bins = pd.cut(grouped_data[feature], bins=6, precision=0)  # Adjust number of bins and precision as needed
+        df = df[(df[feature] != '-') & (df[feature].notna())]
+        df[feature] = df[feature].astype(float)
+        bins = pd.cut(df[feature], bins=6, precision=0)  # Adjust number of bins and precision as needed
         labels = bins.astype(str)
-        grouped_data[feature] = labels
+        df[feature] = labels
+
+    query = f"""
+                SELECT
+                    {feature},
+                    AVG(final_total_points) as 'Average Score',
+                    COUNT(*) as 'Number of Songs'
+                FROM df
+                WHERE qualified_10 = '1' OR qualified_10 = '-'
+                GROUP BY {feature}
+                """
+    grouped_data = sqldf(query, locals())
+
+    grouped_data['Average Score'] = grouped_data['Average Score'].round(2)
 
     grouped_data_sorted = grouped_data.sort_values(by=['Average Score'])
 
